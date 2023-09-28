544050dcbe89fc8ffc51f57e9a3e5d0c584b525c
Samuel Badru <samuelbadru@outlook.com>
2023-09-28 14:31:05 +0100

    createUIElements method implemented

--- src/main/java/com/example/parkinglot/ParkingLotUI.java
+++ src/main/java/com/example/parkinglot/ParkingLotUI.java
@@ -28,11 +28,24 @@ public class ParkingLotUI {
     private void openWindow(){
         createUIElements();
         setUpWindowLayout();
-
     }
 
     private void createUIElements(){
+        entryTimeLabel = new Label("Please enter the date and time the car entered the parking lot");
+        entryTimeText = new TextField();
+        entryTimeText.setPromptText("YYYY-MM-DD HH:mm");
 
+        exitTimeLabel = new Label("Please enter the date and time the car exited the parking lot");
+        exitTimeText = new TextField();
+        exitTimeText.setPromptText("YYYY-MM-DD HH:mm");
+
+        submitButton = new Button("Submit");
+
+        durationResultLabel = new Label("Duration: ");
+        durationResult = new Text();
+
+        feeResultLabel = new Label("Fee: ");
+        feeResult = new Text();
     }
 
     private void setUpWindowLayout(){
