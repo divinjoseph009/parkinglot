6a4d6c5f794242075e7848e90f5213c32831566b
Esra <esra.salem98@gmail.com>
2023-09-28 11:56:07 +0100

    Ticket class v1

--- src/main/java/com/example/parkinglot/Ticket.java
+++ src/main/java/com/example/parkinglot/Ticket.java
@@ -2,31 +2,57 @@ package com.example.parkinglot;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
 
 public class Ticket {
-    LocalDateTime entryTime;
-    LocalDateTime exitTime;
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+    int year =2023;
+    boolean isLeapYear= Year.of(year).isLeap();
 
-    Ticket(LocalDateTime entryTime, LocalDateTime exitTime){
-        this.entryTime = entryTime;
-        this.exitTime = exitTime;
+//    Ticket(LocalDateTime entryTime, LocalDateTime exitTime){
+//        this.entryTime = entryTime;
+//        this.exitTime = exitTime;
+//
+//    }
 
+    //parse the strings that the user inputs into the correct int format.
+    public void setEntryTime(String entryTime){
+        this.entryTime=LocalDateTime.parse(entryTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+    public void setExitTime(String exitTime){
+        this.exitTime=LocalDateTime.parse(exitTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
     //Calculate Duration from Entry and Exit Time
     public int getDurationDays() {
-        return Duration.between(entryTime,exitTime).toDays();
+        return (int) Duration.between(entryTime,exitTime).toDays();
     }
 
     public int getDurationHours() {
-        return Duration.between(entryTime,exitTime).toHours();
+        return (int) Duration.between(entryTime,exitTime).toHours();
     }
 
     public int getDurationMinutes() {
-        return Duration.between(entryTime,exitTime).toMinutes();
+        return (int) Duration.between(entryTime,exitTime).toMinutes();
     }
 
 
+//method to calculate duration on the first day
+    public int calculateDurationOnFirstDay(){
+        int minutes=getDurationMinutes();
+        if (minutes<=30){
+            return 0;
+        }
+        return minutes;
+    }
+    
+    ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("America/New_York"));
+    //boolean isDaylightSavings=zonedDateTime.isDaylightSavings();
 
 
 
