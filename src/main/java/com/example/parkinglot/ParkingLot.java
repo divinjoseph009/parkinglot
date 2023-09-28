cbb8d8ee350cf730aff3ed27393b6a37bcbfacf6
Samuel Badru <samuelbadru@outlook.com>
2023-09-28 15:58:57 +0100

    Calculate Fees change

--- src/main/java/com/example/parkinglot/ParkingLot.java
+++ src/main/java/com/example/parkinglot/ParkingLot.java
@@ -2,24 +2,28 @@ package com.example.parkinglot;
 
 public class ParkingLot {
     Ticket parkingTicket = new Ticket();
+    int durationDays = parkingTicket.getDurationDays();
+    int durationHours = parkingTicket.getDurationHours();
+    int durationMinutes = parkingTicket.getDurationMinutes();
 
     private double calculateFees() {
-        int durationDays = parkingTicket.getDurationDays();
-        int durationHours = parkingTicket.getDurationHours();
-        int durationMinutes = parkingTicket.getDurationMinutes();
+        int fee = 0;
 
-        if(durationDays==0 && durationHours==0 && durationMinutes<=30) {
-            return 0;
+        if (durationDays == 0 && durationHours == 0 && durationMinutes <= 30) {
+            return fee;
+        } else {
+            fee += 15 * durationDays;
+            int remainingHours = durationHours - (durationDays * 24);
+            int remainingMins = durationMinutes - (durationDays * 1440) - (durationHours * 60);
+            int lastDaysFee = 2 * remainingHours;
+
+            if (remainingMins > 0) {
+                lastDaysFee += 2;
+            }
+
+            fee += Math.min(lastDaysFee, 15);
+
+            return fee;
         }
-        int totalHours = (durationDays * 24) + durationHours;
-        int totalFee = totalHours * 2;
-
-        if(totalFee > 15 && durationDays>=1) {
-            totalFee = 15 * durationDays ;
-            return totalFee;
-        } else if (totalFee > 15) {
-            totalFee = 15;
-            return totalFee;
-        } else return totalFee;
     }
 }
