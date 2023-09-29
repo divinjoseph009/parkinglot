ba1bcb92d85718fae50ae4f8f27625b09cb5f2bc
Esra <esra.salem98@gmail.com>
2023-09-29 16:32:05 +0100

    figuring button configurations

--- src/main/java/com/example/parkinglot/ParkingLotUI.java
+++ src/main/java/com/example/parkinglot/ParkingLotUI.java
@@ -28,58 +28,61 @@ public class ParkingLotUI {
     Text feeResult;
 
 
-    public ParkingLotUI(){
-        this.stage = stage;
-        openWindow();
-    }
-
-    private void openWindow(){
-        createUIElements();
-        setUpWindowLayout();
-    }
-
-    private void createUIElements(){
-        entryTimeLabel = new Label("Please enter the date and time the car entered the parking lot");
-
-        entryDateText = new TextField();
-        entryDateText.setPromptText("YYYY-MM-DD");
-
-        entryTimeText = new TextField();
-        entryTimeText.setPromptText("HH:mm");
-
-        exitTimeLabel = new Label("Please enter the date and time the car exited the parking lot");
-
-        exitDateText = new TextField();
-        exitDateText.setPromptText("YYYY-MM-DD");
-
-        exitTimeText = new TextField();
-        exitTimeText.setPromptText("HH:mm");
-
-        submitButton = new Button("Submit");
-        submitButton.setStyle("-fx-font-size: 14px; -fx-background-color: #4CAF50; -fx-text-fill: white;");
-
-        durationResultLabel = new Label("Duration: ");
-
-        durationResult = new Text();
-        durationResult.setStyle("-fx-font-size: 14px; -fx-text-fill: white;");
-
-        feeResultLabel = new Label("Fee: ");
-
-        feeResult = new Text();
-        feeResult.setStyle("-fx-font-size: 14px; -fx-text-fill: white;");
-    }
-
-    private void setUpWindowLayout(){
-        VBox vBox = new VBox(20, entryTimeLabel,entryDateText, entryTimeText, exitTimeLabel, exitDateText, exitTimeText, submitButton, durationResultLabel, durationResult, feeResultLabel, feeResult);
-        vBox.setPadding(new Insets(20));
-        vBox.setAlignment(Pos.CENTER);
-        BackgroundFill backgroundFill = new BackgroundFill(javafx.scene.paint.Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY);
-        vBox.setBackground(new Background(backgroundFill));
-        Scene scene = new Scene(vBox, 400, 400);
-        stage.setTitle("Parking Lot Management");
-        stage.setScene(scene);
-        stage.show();
-    }
+//    public ParkingLotUI(Stage stage){
+//        this.stage = stage;
+//        openWindow();
+//        if(this.stage != null){
+//            System.out.println("setting the stage worked");
+//        }
+//    }
+//
+//    private void openWindow(){
+//        createUIElements();
+//        setUpWindowLayout();
+//    }
+//
+//    private void createUIElements(){
+//        entryTimeLabel = new Label("Please enter the date and time the car entered the parking lot");
+//
+//        entryDateText = new TextField();
+//        entryDateText.setPromptText("YYYY-MM-DD");
+//
+//        entryTimeText = new TextField();
+//        entryTimeText.setPromptText("HH:mm");
+//
+//        exitTimeLabel = new Label("Please enter the date and time the car exited the parking lot");
+//
+//        exitDateText = new TextField();
+//        exitDateText.setPromptText("YYYY-MM-DD");
+//
+//        exitTimeText = new TextField();
+//        exitTimeText.setPromptText("HH:mm");
+//
+//        submitButton = new Button("Submit");
+//        submitButton.setStyle("-fx-font-size: 14px; -fx-background-color: #4CAF50; -fx-text-fill: white;");
+//
+//        durationResultLabel = new Label("Duration: ");
+//
+//        durationResult = new Text();
+//        durationResult.setStyle("-fx-font-size: 14px; -fx-text-fill: white;");
+//
+//        feeResultLabel = new Label("Fee: ");
+//
+//        feeResult = new Text();
+//        feeResult.setStyle("-fx-font-size: 14px; -fx-text-fill: white;");
+//    }
+//
+//    private void setUpWindowLayout(){
+//        VBox vBox = new VBox(20, entryTimeLabel,entryDateText, entryTimeText, exitTimeLabel, exitDateText, exitTimeText, submitButton, durationResultLabel, durationResult, feeResultLabel, feeResult);
+//        vBox.setPadding(new Insets(20));
+//        vBox.setAlignment(Pos.CENTER);
+//        BackgroundFill backgroundFill = new BackgroundFill(javafx.scene.paint.Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY);
+//        vBox.setBackground(new Background(backgroundFill));
+//        Scene scene = new Scene(vBox, 400, 400);
+//        stage.setTitle("Parking Lot Management");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
     public String getUserEntryTime() { return  entryTimeText.getText(); }
     public String getUserEntryDate() { return  entryDateText.getText(); }
