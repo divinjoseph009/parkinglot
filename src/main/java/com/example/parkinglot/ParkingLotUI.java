292ad1ae4c0608af11c9fda4742a6ad2db1f1a7e
Samuel Badru <samuelbadru@outlook.com>
2023-09-29 13:56:24 +0100

    Kadri's frontend changes

--- src/main/java/com/example/parkinglot/ParkingLotUI.java
+++ src/main/java/com/example/parkinglot/ParkingLotUI.java
@@ -1,10 +1,14 @@
 package com.example.parkinglot;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -34,24 +38,35 @@ public class ParkingLotUI {
 
     private void createUIElements(){
         entryTimeLabel = new Label("Please enter the date and time the car entered the parking lot");
+
         entryTimeText = new TextField();
         entryTimeText.setPromptText("YYYY-MM-DD HH:mm");
 
         exitTimeLabel = new Label("Please enter the date and time the car exited the parking lot");
+
         exitTimeText = new TextField();
         exitTimeText.setPromptText("YYYY-MM-DD HH:mm");
 
         submitButton = new Button("Submit");
+        submitButton.setStyle("-fx-font-size: 14px; -fx-background-color: #4CAF50; -fx-text-fill: white;");
 
         durationResultLabel = new Label("Duration: ");
+
         durationResult = new Text();
+        durationResult.setStyle("-fx-font-size: 14px; -fx-text-fill: white;");
 
         feeResultLabel = new Label("Fee: ");
+
         feeResult = new Text();
+        feeResult.setStyle("-fx-font-size: 14px; -fx-text-fill: white;");
     }
 
     private void setUpWindowLayout(){
         VBox vBox = new VBox(20, entryTimeLabel, entryTimeText, exitTimeLabel, exitTimeText, submitButton, durationResultLabel, durationResult, feeResultLabel, feeResult);
+        vBox.setPadding(new Insets(20));
+        vBox.setAlignment(Pos.CENTER);
+        BackgroundFill backgroundFill = new BackgroundFill(javafx.scene.paint.Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY);
+        vBox.setBackground(new Background(backgroundFill));
         Scene scene = new Scene(vBox, 400, 400);
         stage.setTitle("Parking Lot Management");
         stage.setScene(scene);
